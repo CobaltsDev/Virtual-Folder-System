511ac276f0c1ace67d188961707dde697fc98c30
a_Sharp_a <izzymacster@gmail.com>
2017-08-16 19:15:23 -0700

    Finally got the folder adding system down with perfect precision.

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -9,9 +9,9 @@ namespace ConsoleApplication1
 {
     public class Query
     {
+        static List<string> Vpath = new List<string> { "vfsbox" };
         static string XMLPath = "C:/Users/Izzy/Source/Repos/Virtual-Folder-System/ConsoleApplication1/ConsoleApplication1/vfsbox.xml";
         XDocument Doc = XDocument.Load(XMLPath);
-        string Scope = "vsfbox"; 
         string Command = "";
         public Query(string command)
         {
@@ -22,24 +22,89 @@ namespace ConsoleApplication1
             switch(Command)
             {
                 case "New":
-                    Console.WriteLine("%>  Folder");
+                    Console.WriteLine("%>  Folder   :   Alias");
                     Console.Write("$> ");
                     string input = Console.ReadLine();
                     if (input == "Folder" || input == "folder")
                     {
-                        Console.Write("%>  Name?");
-                        Console.WriteLine("");
+                        int atList = 1;
+                        Console.WriteLine("%>  Name?");
                         Console.Write("$> ");
-                        XElement newElement = new XElement(Console.ReadLine());
-                        newElement.Add(new XAttribute("Type", "Folder"));
-                        newElement.Add(new XElement("ROOT"));
-                        Doc.Element(Scope).Add(newElement);
-                        Doc.Save(XMLPath);
+                        string nameInput = Console.ReadLine();
+                        XElement newElement = Doc.Descendants("vfsbox").FirstOrDefault();
+                        while(atList < Vpath.Count)
+                        {
+                            newElement = Doc.Descendants(Vpath[atList]).FirstOrDefault();
+                            atList++;
+                        }
+                        XElement toAdd = new XElement(nameInput);
+                        toAdd.Add(new XAttribute("Type", "Folder"));
+                        newElement.Add(toAdd);
+                        XElement toSave;
+                        if (newElement.Parent == null)
+                        {
+                            toSave = Doc.Element("vfsbox");
+                        }
+                        else
+                        {
+                            toSave = newElement.Parent;
+                        }
+                        while (toSave != Doc.Element("vfsbox"))
+                        {
+                            toSave = toSave.Parent;
+                        }
+                        toSave.Save(XMLPath);
+                        Vpath.Add(nameInput);
+                        Program.Terminal();
+                    }
+                    else if (input == "Alias" || input == "alias")
+                    {
+                        if(Vpath[Vpath.Count - 1] == "vsfbox")
+                        {
+                            Console.WriteLine("I'm sorry, you cannot have file aliases in the root vdirectory. Please create or open a folder");
+                            Program.Terminal();
+                        }
+                        else
+                        {
+                            Console.WriteLine("%>  Description?");
+                            Console.Write("$> ");
+                            string name = Console.ReadLine();
+                            XElement newElement = Doc.Descendants(Vpath[Vpath.Count - 1]).FirstOrDefault();
+                            newElement.Add(new XElement(name));
+                            newElement = Doc.Descendants(name).FirstOrDefault();
+                            newElement.Add(new XAttribute("Type","Alias"));
+                            Console.WriteLine("%>  Path?");
+                            Console.Write("$> ");
+                            string inputPath = Console.ReadLine();
+                            newElement.Add(new XElement("Path", inputPath.Replace(@"\", "/")));
+                            Random rng = new Random();
+                            newElement.Add(new XElement("Index", ConvertToBase35(rng.Next(1, 36)) + ConvertToBase35(rng.Next(1, 36)) + ConvertToBase35(rng.Next(1, 36))));
+                            XElement toSave = Doc.Descendants(Vpath[Vpath.Count - 1]).FirstOrDefault();
+                            toSave.Add(newElement);
+                            toSave.Save(XMLPath);
+                            Program.Terminal();
+                        }
                     }
                     break;
+                
                 default:
                     Program.Terminal();
                     break;
+            }
+        }
+        string ConvertToBase35(int from)
+        {
+            if(from <= 9)
+            {
+                return from.ToString(); ;
+            }
+            else
+            {
+                from -= 9;
+                from = 26 - from;
+                int ascii = 065 + from;
+                char convAscii = (char)ascii;
+                return convAscii.ToString();
             }
         }
     }
